79f9790f84e40616e54be6a28125e2cea98e6d96
CatL <catherinelui.2@gmail.com>
2020-11-01 16:27:15 -0700

    updated servicework.js to serviceworker again and ES6 functions in serviceworker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -9,7 +9,7 @@ const FILES_TO_CACHE = [
     "styles.css",
     "manifest.webmanifest",
     "icons/icon-192x192.png",
-    "icons/icon-512x512.png",
+    "icons/icon-512x512.png"
 ];
 
 // install
@@ -17,7 +17,7 @@ self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
             console.log("Your files were pre-cached successfully!");
-            return cache.addAll(staticFilesToPreCache);
+            return cache.addAll(FILES_TO_CACHE);
         })
     );
 
@@ -43,29 +43,28 @@ self.addEventListener("activate", function (evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function (evt) {
-    const { url } = evt.request;
-    if (url.includes("/api/transaction") || url.includes("/")) {
+self.addEventListener("fetch", evt => {
+    if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches
                 .open(DATA_CACHE_NAME)
-                .then((cache) => {
+                .then(cache => {
                     return fetch(evt.request)
-                        .then((response) => {
+                        .then(response => {
                             // If the response was good, clone it and store it in the cache.
                             if (response.status === 200) {
-                                cache.put(evt.request, response.clone());
+                                cache.put(evt.request.url, response.clone());
                             }
 
                             return response;
                         })
-                        .catch((err) => {
+                        .catch(err => {
                             // Network request failed, try to get it from the cache.
                             return cache.match(evt.request);
-                        });
+                        })
                 })
-                .catch((err) => console.log(err))
-    );
+                .catch(err => console.log(err))
+    )
         return
     }
-});
+})
