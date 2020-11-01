33457182d08a56df9ca9d973af1303042264ee80
CatL <catherinelui.2@gmail.com>
2020-11-01 15:28:13 -0700

    Deploying atlas to heroku

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect("process.env.MONGODB_URI", {
   useNewUrlParser: true,
+  useUnifiedTopology:true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
